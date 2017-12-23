8e04d6fa2b40be5d77690812544b012c91de837a
minjaeL <lee930422@naver.com>
2017-12-23 17:24:00 +0900

    #1 스프링 프로젝트 생성

---
+++ src/main/java/com/takefour/themoment/themoment/TheMomentApplication.java
@@ -0,0 +1,12 @@
+package com.takefour.themoment.themoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMomentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/takefour/themoment/themoment/TheMomentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.takefour.themoment.themoment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheMomentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
