c075015322606e39d751f3697072fe8d0a5decd6
hanbyeol.kim <hanbyeol.kim@navercorp.com>
2017-12-28 22:56:51 +0900

    #26 Repository에 대한 테스트 케이스 추가

--- src/test/java/com/takefour/themoment/themoment/TheMomentApplicationTests.java
+++ src/test/java/com/takefour/themoment/themoment/TheMomentApplicationTests.java
@@ -1,16 +1,56 @@
 package com.takefour.themoment.themoment;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.takefour.themoment.themoment.model.Moment;
+import com.takefour.themoment.themoment.model.User;
+import com.takefour.themoment.themoment.repository.MomentRepository;
+import com.takefour.themoment.themoment.repository.UserRepository;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class TheMomentApplicationTests {
 
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private MomentRepository momentRepository;
+
 	@Test
-	public void contextLoads() {
+	public void userTest() {
+		User user = new User();
+		user.setEmail("byeol3058@gmail.com");
+		user.setUserName("hanbyeol");
+		user.setPassword("themoment");
+
+		entityManager.persist(user);
+
+		List<User> users = userRepository.findAll();
+		assertThat(users)
+				.isNotEmpty()
+				.hasSize(1)
+				.contains(user);
 	}
 
+	@Test
+	public void momentTest() {
+		Moment moment = new Moment();
+		moment.setId(1);
+		entityManager.persist(moment);
+	}
 }
