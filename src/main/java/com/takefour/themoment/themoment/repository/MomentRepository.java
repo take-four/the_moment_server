e55d413abb2d40b754f2aa3b169ea7ea0ad75404
minjae <lee934022@naver.com>
2017-12-28 22:36:46 +0900

    #26 Crud를 Jpa로 바꿈

--- src/main/java/com/takefour/themoment/themoment/repository/MomentRepository.java
+++ src/main/java/com/takefour/themoment/themoment/repository/MomentRepository.java
@@ -1,9 +1,9 @@
 package com.takefour.themoment.themoment.repository;
 
 import com.takefour.themoment.themoment.model.Moment;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MomentRepository extends CrudRepository<Moment,Integer> {
+public interface MomentRepository extends JpaRepository<Moment,Integer> {
 }
 
 //<modelname, primary key type>
--- src/main/java/com/takefour/themoment/themoment/repository/UserRepository.java
+++ src/main/java/com/takefour/themoment/themoment/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.takefour.themoment.themoment.repository;
 
 import com.takefour.themoment.themoment.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User,Integer> {
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserId(String userId);
+    User findByUserIdAndPassword(String userId, String password);
 }
