af904800620551f6f8707257d545b7afaee5d3a3
hanbyeol.kim <hanbyeol.kim@navercorp.com>
2018-01-03 23:34:15 +0900

    #31 Controller 패키지 만들기

---
+++ src/main/java/com/takefour/themoment/themoment/controller/api/MomentController.java
@@ -0,0 +1,9 @@
+package com.takefour.themoment.themoment.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apis/moments")
+public class MomentController {
+}
