0a16eaba72910c9e74746c111779b17c9768a9d3
minjae <lee934022@naver.com>
2018-01-05 20:24:48 +0900

    #27 service만들기

--- src/main/java/com/takefour/themoment/themoment/repository/UserRepository.java
+++ src/main/java/com/takefour/themoment/themoment/repository/UserRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	User findByUserName(String userName);
+	User findByEmail(String email);
 
-	User findByUserNameAndPassword(String userName, String password);
+	User findByEmailAndPassword(String email, String password);
+
+	User update(Integer id);
 }
